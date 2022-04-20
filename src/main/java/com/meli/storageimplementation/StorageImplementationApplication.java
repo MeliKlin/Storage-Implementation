01c2b857195e21fe9b8bb75469e1934efca56514
Klinton Sae Hyung Lee <klinton.lee@mercadolivre.com>
2022-04-20 11:57:25 -0300

    chore: add spring initial structure

---
+++ src/main/java/com/meli/storageimplementation/StorageImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.meli.storageimplementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageImplementationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorageImplementationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meli/storageimplementation/StorageImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.storageimplementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageImplementationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
