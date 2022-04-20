65d6b2b04f24623b1345330c14a0c09fc9179b6a
Klinton Sae Hyung Lee <klinton.lee@mercadolivre.com>
2022-04-20 12:23:01 -0300

    feat: implement Jewel model with Entity annotation

---
+++ src/main/java/com/meli/storageimplementation/models/Jewel.java
@@ -0,0 +1,31 @@
+package com.meli.storageimplementation.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Jewel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(updatable = false, nullable = false)
+    UUID id;
+    String material;
+    BigDecimal weight;
+    Integer karat;
+
+}
