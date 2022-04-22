cf6824048a489fe00f76f1d7152e0521fa4d2f61
Klinton Sae Hyung Lee <klinton.lee@mercadolivre.com>
2022-04-22 00:37:15 -0300

    feat: implement error handler to not valid arguments

---
+++ src/main/java/com/meli/storageimplementation/error/ValidationErrorDTO.java
@@ -0,0 +1,18 @@
+package com.meli.storageimplementation.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ValidationErrorDTO {
+
+    private String message;
+    private List<String> errors;
+
+}
